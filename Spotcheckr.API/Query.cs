40a5f8089bb3763b0000253c26d4374a80658036
Nitish Sachar <nitish.sachar@protonmail.com>
2020-10-31 00:22:02 -0700

    init

---
+++ Spotcheckr.API/Query.cs
@@ -0,0 +1,12 @@
+﻿namespace Spotcheckr.API
+{
+    public class Query
+    {
+        public Greetings GetGreetings() => new Greetings();
+    }
+
+    public class Greetings
+    {
+        public string Hello() => "World";
+    }
+}
