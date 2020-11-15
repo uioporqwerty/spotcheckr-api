16ab628ff57e2cf14064f79586159a83771b7784
Nitish Sachar <nitish.sachar@protonmail.com>
2020-11-15 02:12:09 -0800

    add initial unit tests project

---
+++ Spotcheckr.API.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Spotcheckr.API.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
