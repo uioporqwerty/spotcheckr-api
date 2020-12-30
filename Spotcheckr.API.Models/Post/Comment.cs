0ca346896e812e4b121d7f150b89d2ccb89854e4
uioporqwerty <nitish.sachar@protonmail.com>
2020-12-29 19:03:03 -0800

    add models for exercise post amd comments

---
+++ Spotcheckr.API.Models/Post/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Spotcheckr.API.Models
+{
+	/// <summary>
+	/// Represents responses by users on an exercise post.
+	/// </summary>
+	public class Comment
+	{
+		/// <summary>
+		/// Text of the comment.
+		/// </summary>
+		public string Text { get; set; }
+
+		/// <summary>
+		/// Date comment was modified. Defaults to created date if unmodified.
+		/// </summary>
+		public DateTime ModifiedDate { get; set; }
+
+		/// <summary>
+		/// Date comment was created.
+		/// </summary>
+		public DateTime CreatedDate { get; set; }
+
+		/// <summary>
+		/// User that created the comment.
+		/// </summary>
+		public IUser CreatedBy { get; set; }
+	}
+}
---
+++ Spotcheckr.API.Models/Post/ExercisePost.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spotcheckr.API.Models
+{
+	/// <summary>
+	/// An exercise post represents an athlete post a user would like information about.
+	/// </summary>
+	public class ExercisePost
+	{
+		/// <summary>
+		/// Unique identifier for the exercise post.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Title of the exercise post.
+		/// </summary>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// Detailed description of the exercise post.
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Date the exercise post was modified. Defaults to Date Created when unmodified. 
+		/// </summary>
+		public DateTime ModifiedDate { get; set; }
+
+		/// <summary>
+		/// Date exercise post created.
+		/// </summary>
+		public DateTime CreatedDate { get; set; }
+
+		/// <summary>
+		/// User that created the exercise post.
+		/// </summary>
+		public IUser CreatedBy { get; set; }
+
+		/// <summary>
+		/// Comments related to the exercise post.
+		/// </summary>
+		public IEnumerable<Comment> Comments { get; set; }
+	}
+}
